aae08085b567de75aeed458a4a0d5c874ed2b07b
DannyChung9322 <103354763+DannyChung9322@users.noreply.github.com>
2022-04-10 23:05:35 +0800

    Delete src directory

--- src/main/java/com/candidate/dannychung/progchallenge/constant/ConstantValue.java
+++
@@ -1,5 +0,0 @@
-package com.candidate.dannychung.progchallenge.constant;
-
-public class ConstantValue {
-	public static final double riskFreeInterestRate = 0.04;
-}
--- src/main/resources/dml.sql
+++
@@ -1,9 +0,0 @@
-insert into Stock_and_option_details values ('0005.HK','HSBC',1,0.2,0.3,null,0,53.95,0,0,0);
-insert into Stock_and_option_details values ('50001.HK','HSBC Call Option',2,0.00,0.3,'0005.HK',1,53.95,52,0,0);
-insert into Stock_and_option_details values ('50002.HK','HSBC Put Option',3,0.00,0.3,'0005.HK',1,53.95,52,0,0);
-insert into Stock_and_option_details values ('3988.HK','BOC HK',1,0.2,0.15,null,0,3.14,0,0,0);
-insert into Stock_and_option_details values ('60001.HK','BOC HK Call Option',2,0.00,0.15,'3988.HK',1,3.14,3.2,0,0);
-insert into Stock_and_option_details values ('60002.HK','BOC HK Put Option',3,0.00,0.15,'3988.HK',1,3.14,3.2,0,0);
-insert into Stock_and_option_details values ('0023.HK','HKBEA',1,0.3,0.15,null,0,12.22,0,0,0);
-insert into Stock_and_option_details values ('70001.HK','HKBEA Call Option',2,0.00,0.15,'0023.HK',0.5,12.22,11.89,0,0);
-insert into Stock_and_option_details values ('70002.HK','HKBEA Put Option',3,0.00,0.15,'0023.HK',0.5,12.22,11.89,0,0);
